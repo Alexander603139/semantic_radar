9963e71d3ea2427369e28cf66273ce2cb0f33711
nikihater <88324592+nikihater@users.noreply.github.com>
2026-08-02 19:58:49 +0300

    modified:   services/storage/src/s3_client.py

--- services/storage/src/s3_client.py
+++ services/storage/src/s3_client.py
@@ -1,6 +1,6 @@
 import boto3
-from botocore.exceptions import ClientError
 import logging
+from botocore.exceptions import ClientError
 from .settings import settings
 
 logger = logging.getLogger(__name__)
